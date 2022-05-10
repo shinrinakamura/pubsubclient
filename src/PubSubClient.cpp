47c7e7b25cfd662a4e8b4a360be0aec5ad75b396
S.Nakamura <shinri.nakamura0@gmail.com>
2022-05-10 13:42:18 +0900

    コンフリクトの発生確認

--- src/PubSubClient.cpp
+++ src/PubSubClient.cpp
@@ -370,7 +370,7 @@ uint32_t PubSubClient::readPacket(uint8_t* lengthLength) {
 
 boolean PubSubClient::loop() {
     if (connected()) {  //接続が確率したら
-        unsigned long t = millis();         //計算用の時間を取得？
+        unsigned long t = millis();         //計算用の時間を取得？コンフリクトを起こさせる  
         //時間の計算
         if ((t - lastInActivity > this->keepAlive*1000UL) || (t - lastOutActivity > this->keepAlive*1000UL)) {
             if (pingOutstanding) {
